970463d33091c7d2c1f9e4d48dc06ba3285013a4
Steven Barlow <stemail23@gmail.com>
2014-04-08 22:23:29 +1000

    Implemented basic #ping() for mock, couchbase, couchDB, and pouchDB

--- test/couchlike/couchlike.spec.js
+++ test/couchlike/couchlike.spec.js
@@ -1,8 +1,78 @@
 var should = require("should");
 var couchlike = require('./../../lib/couchlike.js');
 
+function check(done, checkFunction) {
+	try {
+		checkFunction();
+		done();
+	} catch(error) {
+		done(error);
+	}
+}
+
 describe('couchlike', function(){
 	it('should exist', function(){
 		should.exist(couchlike);
+		should.exist(couchlike.Couchlike);
 	});
 });
+
+function testWithConfig(configSpec) {
+	describe('an instance of Couchlike with config '+configSpec.name, function(){
+		var couch;
+
+		before(function() {
+			couch = new couchlike.Couchlike(configSpec.config);
+		});
+
+		it('should be possible', function(){
+			should.exist(couch);
+		});
+
+		describe('#ping', function() {
+			it('should succeed', function(done){
+				couch.ping(function(err, result) {
+					check(done, function() {
+						should.not.exist(err);
+						should.exist(result);
+					});
+				});
+			});
+		});
+	});
+}
+
+var configs = {
+	'null': null,
+	couchbaseConfig: {
+		couchlike: {
+			type: couchlike.engineType.couchbase
+		},
+		config: {
+			host: 'localhost:8091',
+			bucket: 'test'
+		}
+	},
+	couchDB: {
+		couchlike: {
+			type: couchlike.engineType.couchDB
+		},
+		config: {
+			host: 'http://localhost:5984'
+		}
+	},
+	pouchDB: {
+		couchlike: {
+			type: couchlike.engineType.pouchDB
+		},
+		config: {
+			database: 'testdata'
+		}
+	}
+};
+
+for (var config in configs) {
+	var test = {name: config, config: configs[config]};
+	testWithConfig(test);
+}
+
