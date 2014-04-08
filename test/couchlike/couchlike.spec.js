e85f3b9017cb9b2b65fae1b18a5fa7091350e318
Steven Barlow <stemail23@gmail.com>
2014-04-08 14:05:36 +1000

    basic skeleton, with test stubs

---
+++ test/couchlike/couchlike.spec.js
@@ -0,0 +1,8 @@
+var should = require("should");
+var couchlike = require('./../../lib/couchlike.js');
+
+describe('couchlike', function(){
+	it('should exist', function(){
+		should.exist(couchlike);
+	});
+});
